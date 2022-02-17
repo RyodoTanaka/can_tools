3de671b2bfee12a4c1c4040d6270252379907c8e
Ryodo Tanaka <groadpg@gmail.com>
2022-02-17 21:45:18 +0900

    update to get baud rate as 2nd argument

--- peakcan_setup.bash
+++ peakcan_setup.bash
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo ip link set $1 down
-sudo ip link set $1 type can bitrate 250000
-# sudo ip link set $1 type can bitrate 500000
+sudo ip link set $1 type can bitrate $2
 sudo ip link set $1 name $1
 sudo ip link set $1 up
 sudo ifconfig $1 txqueuelen 100000
